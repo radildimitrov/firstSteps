711af7cf58a51bfd31378ca2b1befe8f0729472b
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-02-09 22:17:37 +0200

    Create 05. Account Balance.js

---
+++ Programming Basics with JavaScript/11. While-loops/05. Account Balance.js
@@ -0,0 +1,20 @@
+function accountBalance(input) {
+    
+    let deposit = Number(input[0]);
+    let balance = 0;
+    let index = 1;
+    while (deposit !== "NoMoreMoney"){
+        let amount = Number(deposit);
+        if (amount < 0){
+            console.log(`Invalid operation!`);
+            break;
+        }
+        balance += amount;
+        console.log(`Increase: ${amount.toFixed(2)}`);
+        deposit = input[index];
+        index++;
+    }
+    console.log(`Total: ${balance.toFixed(2)}`);
+
+}
+accountBalance(["120", "45.55", "-150"])
