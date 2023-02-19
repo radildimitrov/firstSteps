883c079212f3f7c7a9bb08c5832726484581303e
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-02-19 10:42:49 +0200

    Create 04. Foreign Languages.js

---
+++ Programming Fundamentals with JavaScript/5. Basic Syntax, Conditional Statements and Loops/04. Foreign Languages.js
@@ -0,0 +1,22 @@
+function foreignLanguages(country) {
+  switch (country) {
+    case "England":
+      console.log("English");
+      break;
+    case "USA":
+      console.log("English");
+      break;
+    case "Spain":
+      console.log("Spanish");
+      break;
+    case "Argentina":
+      console.log("Spanish");
+      break;
+    case "Mexico":
+      console.log("Spanish");
+      break;
+    default:
+      console.log("unknown");
+  }
+}
+foreignLanguages('USA');
