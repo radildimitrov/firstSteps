5ea3180db0de31617a4344c262bc7d2e624bcc6e
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-02-28 08:18:23 +0200

    Create 01. Echo Type.js

---
+++ Programming Fundamentals with JavaScript/8. Data Types and Variables/01. Echo Type.js
@@ -0,0 +1,10 @@
+function echoType(param) {
+  let type = typeof param;
+  console.log(type);
+  if (type === "string" || type === "number") {
+    console.log(param);
+  } else {
+    console.log("Parameter is not suitable for printing");
+  }
+}
+echoType(18);
