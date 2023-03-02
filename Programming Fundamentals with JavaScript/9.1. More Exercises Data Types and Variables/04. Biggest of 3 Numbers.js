e4f3366959942eda8ac7e1499af8b0d56a8cfd0f
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-03-02 22:45:47 +0200

    Create 04. Biggest of 3 Numbers.js

---
+++ Programming Fundamentals with JavaScript/9.1. More Exercises Data Types and Variables/04. Biggest of 3 Numbers.js
@@ -0,0 +1,16 @@
+function biggestOfThreeNumbers(a, b, c) {
+  if (a >= b && a >= c) {
+    console.log(a);
+  } else if (b >= a && b >= c) {
+    console.log(b);
+  } else {
+    console.log(c);
+  }
+}
+biggestOfThreeNumbers(
+  2,
+
+  2,
+
+  2
+);
