43823347eacb60dc4335f467e297824111ba8d2d
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-03-26 23:53:32 +0300

    Create 06. Sign Check.js

---
+++ Programming Fundamentals with JavaScript/14. Functions/06. Sign Check.js
@@ -0,0 +1,15 @@
+function signCheck(numberOne, numberTwo, numberThree) {
+  let result = numberOne * numberTwo * numberThree;
+  if (result <= 0) {
+    console.log("Negative");
+  } else {
+    console.log("Positive");
+  }
+}
+signCheck(
+  -6,
+
+  -12,
+
+  14
+);
