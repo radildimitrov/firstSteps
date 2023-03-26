4012c9f1e1ebfbf2bdd99421710daf833423aeb0
Radil Dimitrov <radil.dimitrov@gmail.com>
2023-03-26 23:47:11 +0300

    Update 04. Orders.js

--- Programming Fundamentals with JavaScript/14. Functions/04. Orders.js
+++ Programming Fundamentals with JavaScript/14. Functions/04. Orders.js
@@ -1,3 +1,20 @@
-function orders() {
-    
-}orders("water", 5);
+function orders(item, quantity) {
+  let result = 0;
+  switch (item) {
+    case "coffee":
+      result = quantity * 1.5;
+      break;
+    case "water":
+      result = quantity * 1;
+      break;
+    case "coke":
+      result = quantity * 1.4;
+      break;
+    case "snacks":
+      result = quantity * 2;
+      break;
+  }
+
+  console.log(result.toFixed(2));
+}
+orders("water", 5);
